24b9684ef3660170e553c1bde8c41706b19d4cbd
smallGod <davies.mugume@gmail.com>
2023-02-07 16:11:14 +0300

    Execute script from Java code

--- src/main/java/org/openmrs/ohrimamba/Main.java
+++ src/main/java/org/openmrs/ohrimamba/Main.java
@@ -1,7 +1,19 @@
 package org.openmrs.ohrimamba;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ScriptRunner runner = new ScriptRunner();
+        try {
+            runner.execute();
+        } catch (TimeoutException | InterruptedException | ExecutionException | IOException e) {
+            System.err.println("Error!");
+            e.printStackTrace();
+        }
+        return;
     }
 }
--- src/main/java/org/openmrs/ohrimamba/ScriptRunner.java
+++ src/main/java/org/openmrs/ohrimamba/ScriptRunner.java
@@ -1,8 +1,8 @@
 package org.openmrs.ohrimamba;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.concurrent.*;
+import java.util.function.Consumer;
 
 /**
  * @author Arthur M.D
@@ -12,15 +12,35 @@ public class ScriptRunner {
     public void execute() throws IOException, InterruptedException, ExecutionException, TimeoutException {
 
         ProcessBuilder builder = new ProcessBuilder();
+        String homeDir = System.getProperty("user.home");
 
-        builder.command("/bin/bash", "./Users/ayinza/srv/applications/mets/ohri-mamba-plugin/src/main/resources/_core/database/mysql/compile.sh");
-        builder.directory(new File(System.getProperty("user.home")));
+        builder.directory(new File("src/main/resources/_core/database/mysql"));
+        builder.command("sh", "./compile.sh");
+        //builder.command("sh", "-c", "ls");
         Process process = builder.start();
-        StreamGobbler streamGobbler =
-                new StreamGobbler(process.getInputStream(), System.out::println);
+        StreamGobbler streamGobbler = new StreamGobbler(
+                process.getInputStream(), System.out::println);
         Future<?> future = Executors.newSingleThreadExecutor().submit(streamGobbler);
         int exitCode = process.waitFor();
         assert exitCode == 0;
+        System.out.println("exited: " + exitCode);
         future.get(10, TimeUnit.SECONDS);
+        //System.out.println("Successful: " + isSuccessful + ", process is alive: " + process.isAlive());
+    }
+
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
     }
 }
