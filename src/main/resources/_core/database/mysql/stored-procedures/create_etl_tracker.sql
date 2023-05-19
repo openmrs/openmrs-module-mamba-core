4788c5c4b4209d682ff7eac2e6392d933953b8d3
smallGod <davies.mugume@gmail.com>
2023-05-19 14:00:58 +0300

    Adds re-compile flag to build scripts

---
+++ src/main/resources/_core/database/mysql/stored-procedures/create_etl_tracker.sql
@@ -0,0 +1,12 @@
+CREATE TABLE zzmamba_etl_tracker
+(
+    etl_tracker_id           INT                             NOT NULL AUTO_INCREMENT,
+    initial_run_date         DATETIME                        NOT NULL,
+    start_date               DATETIME                        NOT NULL,
+    end_date                 DATETIME                        NOT NULL,
+    time_taken_microsec      BIGINT                          NOT NULL,
+    completion_status        CHAR(10) CHARACTER SET UTF8MB4  NOT NULL,
+    success_or_error_message CHAR(255) CHARACTER SET UTF8MB4 NOT NULL,
+    next_run_date            DATETIME                        NOT NULL,
+    PRIMARY KEY (etl_tracker_id)
+);
