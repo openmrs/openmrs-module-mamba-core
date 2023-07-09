f18ee57fd499dfe3b5edbfe0f08109976d6b11fb
smallGod <davies.mugume@gmail.com>
2023-07-09 13:48:06 +0300

    convert library jar to omod lib

---
+++ omod/src/main/java/org/openmrs/module/Main.java
@@ -0,0 +1,11 @@
+package org.openmrs.module;
+
+/**
+ * @author smallGod date: 09/07/2023
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
