8f5a05a049d1ac8e36c3cea5087889c1ba09fcad
smallGod <davies.mugume@gmail.com>
2023-07-20 10:04:19 +0200

    formatting

--- src/main/resources/_core/database/mysql/dimensions/dim_person/sp_mamba_dim_person_create.sql
+++ src/main/resources/_core/database/mysql/dimensions/dim_person/sp_mamba_dim_person_create.sql
@@ -2,20 +2,20 @@
 
 CREATE TABLE mamba_dim_person
 (
-    id                  INT      NOT NULL AUTO_INCREMENT,
-    person_id           INT      NOT NULL,
-    birthdate           DATE NULL,
-    birthdate_estimated TINYINT  NOT NULL,
-    age                 INT NULL,
-    dead                TINYINT  NOT NULL,
-    death_date          DATETIME NULL,
-    deathdate_estimated TINYINT  NOT NULL,
+    id                  INT          NOT NULL AUTO_INCREMENT,
+    person_id           INT          NOT NULL,
+    birthdate           DATE         NULL,
+    birthdate_estimated TINYINT      NOT NULL,
+    age                 INT          NULL,
+    dead                TINYINT      NOT NULL,
+    death_date          DATETIME     NULL,
+    deathdate_estimated TINYINT      NOT NULL,
     gender              VARCHAR(255) NULL,
-    date_created        DATETIME NOT NULL,
+    date_created        DATETIME     NOT NULL,
     person_name_short   VARCHAR(255) NULL,
-    person_name_long    TEXT NULL,
-    uuid                CHAR(38) NOT NULL,
-    voided              TINYINT  NOT NULL,
+    person_name_long    TEXT         NULL,
+    uuid                CHAR(38)     NOT NULL,
+    voided              TINYINT      NOT NULL,
 
     PRIMARY KEY (id)
 ) CHARSET = UTF8MB4;
--- src/main/resources/_core/database/mysql/dimensions/dim_person/sp_mamba_dim_person_insert.sql
+++ src/main/resources/_core/database/mysql/dimensions/dim_person/sp_mamba_dim_person_insert.sql
@@ -1,38 +1,37 @@
 -- $BEGIN
 
 INSERT INTO mamba_dim_person
-    (
-        person_id,
-        birthdate,
-        birthdate_estimated,
-        age,
-        dead,
-        death_date,
-        deathdate_estimated,
-        gender,
-        date_created,
-        person_name_short,
-        person_name_long,
-        uuid,
-        voided
-    )
+(person_id,
+ birthdate,
+ birthdate_estimated,
+ age,
+ dead,
+ death_date,
+ deathdate_estimated,
+ gender,
+ date_created,
+ person_name_short,
+ person_name_long,
+ uuid,
+ voided)
 
-    SELECT psn.person_id,
-           psn.birthdate,
-           psn.birthdate_estimated,
-           fn_mamba_age_calculator(birthdate,death_date) AS age,
-           psn.dead,
-           psn.death_date,
-           psn.deathdate_estimated,
-           psn.gender,
-           psn.date_created,
-           CONCAT_WS(' ',prefix,given_name,middle_name,family_name) AS person_name_short,
-           CONCAT_WS(' ', prefix,given_name, middle_name,family_name_prefix, family_name,family_name2,family_name_suffix, degree)
-            AS person_name_long,
-            psn.uuid,
-            psn.voided
-    FROM person psn
-     INNER JOIN  person_name pn
-         on psn.person_id = pn.person_id;
+SELECT psn.person_id,
+       psn.birthdate,
+       psn.birthdate_estimated,
+       fn_mamba_age_calculator(birthdate, death_date)               AS age,
+       psn.dead,
+       psn.death_date,
+       psn.deathdate_estimated,
+       psn.gender,
+       psn.date_created,
+       CONCAT_WS(' ', prefix, given_name, middle_name, family_name) AS person_name_short,
+       CONCAT_WS(' ', prefix, given_name, middle_name, family_name_prefix, family_name, family_name2,
+                 family_name_suffix, degree)
+                                                                    AS person_name_long,
+       psn.uuid,
+       psn.voided
+FROM person psn
+         INNER JOIN person_name pn
+                    on psn.person_id = pn.person_id;
 
 -- $END
