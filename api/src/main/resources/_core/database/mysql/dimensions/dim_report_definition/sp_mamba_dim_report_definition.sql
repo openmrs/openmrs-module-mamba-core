7c9ac6bc69b0ca8112e3f22a58b40bd633858dc3
smallGod <davies.mugume@gmail.com>
2023-08-03 23:06:09 +0100

    Adds logic to properly read reports.json file

--- api/src/main/resources/_core/database/mysql/dimensions/dim_report_definition/sp_mamba_dim_report_definition.sql
+++ api/src/main/resources/_core/database/mysql/dimensions/dim_report_definition/sp_mamba_dim_report_definition.sql
@@ -1,7 +1,7 @@
 -- $BEGIN
 
 CALL sp_mamba_dim_report_definition_create();
--- CALL sp_mamba_dim_report_definition_insert();
+CALL sp_mamba_dim_report_definition_insert();
 CALL sp_mamba_dim_report_definition_update();
 
 -- $END
--- api/src/main/resources/_core/database/mysql/dimensions/dim_report_definition/sp_mamba_dim_report_definition_create.sql
+++ api/src/main/resources/_core/database/mysql/dimensions/dim_report_definition/sp_mamba_dim_report_definition_create.sql
@@ -3,12 +3,12 @@
 CREATE TABLE mamba_dim_report_definition
 (
     id                    INT          NOT NULL AUTO_INCREMENT,
-    report_id             VARCHAR(255) NOT NULL,
-    report_procedure_name VARCHAR(255) NOT NULL, -- should be derived from report_id??
+    report_id             VARCHAR(255) NOT NULL UNIQUE,
+    report_procedure_name VARCHAR(255) NOT NULL UNIQUE, -- should be derived from report_id??
     sql_query             TEXT         NOT NULL,
-    table_name            VARCHAR(255) NOT NULL, -- name of the table (will contain columns) of this query
+    table_name            VARCHAR(255) NOT NULL,        -- name of the table (will contain columns) of this query
     report_name           VARCHAR(255) NULL,
-    result_column_names   TEXT         NULL,     -- comma-separated column names
+    result_column_names   TEXT         NULL,            -- comma-separated column names
 
     PRIMARY KEY (id)
 )
@@ -16,7 +16,6 @@ CREATE TABLE mamba_dim_report_definition
 
 CREATE INDEX mamba_dim_report_definition_report_id_index
     ON mamba_dim_report_definition (report_id);
-
 
 
 CREATE TABLE mamba_dim_report_definition_parameters
