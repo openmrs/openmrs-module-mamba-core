eb6ad98e08afabafcb1cd9f4b9d781fef0e732de
smallGod <davies.mugume@gmail.com>
2023-08-10 22:54:39 +0300

    OHRI-1573 Adds report generator wrapper

---
+++ api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_generate_report_wrapper.sql
@@ -0,0 +1,59 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS sp_mamba_generate_report_wrapper;
+
+CREATE PROCEDURE sp_mamba_generate_report_wrapper(IN report_identifier VARCHAR(255), IN parameter_list JSON)
+BEGIN
+
+    DECLARE proc_name VARCHAR(255);
+    DECLARE sql_args VARCHAR(1000);
+    DECLARE arg_name VARCHAR(50);
+    DECLARE arg_value VARCHAR(255);
+    DECLARE tester VARCHAR(255);
+    DECLARE done INT DEFAULT FALSE;
+
+    DECLARE cursor_parameter_names CURSOR FOR
+        SELECT DISTINCT (p.parameter_name)
+        FROM mamba_dim_report_definition_parameters p
+        WHERE p.report_id = report_identifier;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    SET proc_name = (SELECT DISTINCT (rd.report_procedure_name)
+                     FROM mamba_dim_report_definition rd
+                     WHERE rd.report_id = report_identifier);
+
+    OPEN cursor_parameter_names;
+    read_loop:
+    LOOP
+        FETCH cursor_parameter_names INTO arg_name;
+
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        SET arg_value = IFNULL((JSON_EXTRACT(parameter_list, CONCAT('$[', ((SELECT p.parameter_position
+                                                                            FROM mamba_dim_report_definition_parameters p
+                                                                            WHERE p.parameter_name = arg_name
+                                                                              AND p.report_id = report_identifier) - 1),
+                                                                    '].value'))), 'NULL');
+        SET tester = CONCAT_WS(', ', tester, arg_value);
+        SET sql_args = CONCAT_WS(', ', sql_args, arg_value);
+
+    END LOOP;
+
+    CLOSE cursor_parameter_names;
+
+    SET @sql = CONCAT('CALL ', proc_name, '(', sql_args, ')');
+
+    drop table if exists kk;
+    create table kk as
+    select @sql, tester;
+
+    PREPARE stmt FROM @sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+END //
+
+DELIMITER ;
