5dc6e412e2c8617229f60e687eac63c5620a6af6
laureen <laureen.omare@ucgloballprograms.org>
2023-11-10 05:40:55 +0300

    OHRI-1760 Feature - ObsGroups support for MambaETL

--- api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_flat_encounter_obs_group_table_create.sql
+++ api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_flat_encounter_obs_group_table_create.sql
@@ -9,8 +9,9 @@ BEGIN
 
     SET session group_concat_max_len = 20000;
     SET @column_labels := NULL;
+    SET @tbl_obs_group_name = CONCAT(flat_encounter_table_name,'_obs_group');
 
-    SET @drop_table = CONCAT('DROP TABLE IF EXISTS `', flat_encounter_table_name,'_obs_group', '`');
+    SET @drop_table = CONCAT('DROP TABLE IF EXISTS `', @tbl_obs_group_name, '`');
 
     SELECT GROUP_CONCAT(column_label SEPARATOR ' TEXT, ')
     INTO @column_labels
@@ -26,12 +27,9 @@ BEGIN
     WHERE flat_table_name = flat_encounter_table_name
       AND concept_datatype IS NOT NULL;
 
-    IF @column_labels IS NULL THEN
+    IF @column_labels IS NOT NULL THEN
         SET @create_table = CONCAT(
-                'CREATE TABLE `', flat_encounter_table_name,'`_obs_group`', '` (encounter_id INT NOT NULL, client_id INT NOT NULL, encounter_datetime DATETIME NOT NULL, INDEX idx_encounter_id (encounter_id), INDEX idx_client_id (client_id), INDEX idx_encounter_datetime (encounter_datetime));');
-    ELSE
-        SET @create_table = CONCAT(
-                'CREATE TABLE `', flat_encounter_table_name,'`_obs_group`', '` (encounter_id INT NOT NULL, client_id INT NOT NULL, encounter_datetime DATETIME NOT NULL, ', @column_labels, ' TEXT, INDEX idx_encounter_id (encounter_id), INDEX idx_client_id (client_id), INDEX idx_encounter_datetime (encounter_datetime));');
+                'CREATE TABLE `', @tbl_obs_group_name, '` (encounter_id INT NOT NULL, client_id INT NOT NULL, encounter_datetime DATETIME NOT NULL, ', @column_labels, ' TEXT, INDEX idx_encounter_id (encounter_id), INDEX idx_client_id (client_id), INDEX idx_encounter_datetime (encounter_datetime));');
     END IF;
 
 
@@ -39,7 +37,9 @@ BEGIN
     PREPARE createtb FROM @create_table;
 
     EXECUTE deletetb;
-    EXECUTE createtb;
+    IF @column_labels IS NOT NULL THEN
+        EXECUTE createtb;
+    END IF;
 
     DEALLOCATE PREPARE deletetb;
     DEALLOCATE PREPARE createtb;
