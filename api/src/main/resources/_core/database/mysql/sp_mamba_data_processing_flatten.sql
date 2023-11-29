012b017fae07f14276f747655d0cad91ad343b43
laureen <laureen.omare@ucgloballprograms.org>
2023-11-29 08:57:16 +0300

    OHRI-2007 Create Different tables for the multiple Obs Groups

--- api/src/main/resources/_core/database/mysql/sp_mamba_data_processing_flatten.sql
+++ api/src/main/resources/_core/database/mysql/sp_mamba_data_processing_flatten.sql
@@ -40,6 +40,8 @@ CALL sp_mamba_dim_agegroup;
 
 CALL sp_mamba_z_encounter_obs;
 
+CALL sp_mamba_dim_obs_group;
+
 CALL sp_mamba_flat_encounter_table_create_all;
 
 CALL sp_mamba_flat_encounter_table_insert_all;
--- api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_flat_encounter_obs_group_table_insert_all.sql
+++ api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_flat_encounter_obs_group_table_insert_all.sql
@@ -7,26 +7,50 @@ CREATE PROCEDURE sp_mamba_flat_encounter_obs_group_table_insert_all()
 BEGIN
 
     DECLARE tbl_name CHAR(50) CHARACTER SET UTF8MB4;
+    DECLARE obs_name CHAR(50) CHARACTER SET UTF8MB4;
 
-    DECLARE done INT DEFAULT FALSE;
+    DECLARE done INT DEFAULT 0;
 
     DECLARE cursor_flat_tables CURSOR FOR
-        SELECT DISTINCT(flat_table_name) FROM mamba_dim_concept_metadata;
+    SELECT DISTINCT(flat_table_name) FROM mamba_dim_concept_metadata;
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    DECLARE cursor_obs_group_tables CURSOR FOR
+    SELECT DISTINCT(obs_group_name) FROM mamba_dim_obs_group;
+
+    -- DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
     OPEN cursor_flat_tables;
-    computations_loop:
-    LOOP
-        FETCH cursor_flat_tables INTO tbl_name;
 
-        IF done THEN
-            LEAVE computations_loop;
-        END IF;
+        REPEAT
+            FETCH cursor_flat_tables INTO tbl_name;
+                IF NOT done THEN
+                    OPEN cursor_obs_group_tables;
+                        block2: BEGIN
+                            DECLARE doneobs_name INT DEFAULT 0;
+                            DECLARE firstobs_name varchar(255) DEFAULT '';
+                            DECLARE i int DEFAULT 1;
+                            DECLARE CONTINUE HANDLER FOR NOT FOUND SET doneobs_name = 1;
 
-        CALL sp_mamba_flat_encounter_obs_group_table_insert(tbl_name);
+                            REPEAT
+                                FETCH cursor_obs_group_tables INTO obs_name;
 
-    END LOOP computations_loop;
+                                    IF i = 1 THEN
+                                        SET firstobs_name = obs_name;
+                                    END IF;
+
+                                    CALL sp_mamba_flat_encounter_obs_group_table_insert(tbl_name,obs_name);
+                                    SET i = i + 1;
+
+                                UNTIL doneobs_name
+                            END REPEAT;
+
+                            CALL sp_mamba_flat_encounter_obs_group_table_insert(tbl_name,firstobs_name);
+                        END block2;
+                    CLOSE cursor_obs_group_tables;
+            END IF;
+                        UNTIL done
+        END REPEAT;
     CLOSE cursor_flat_tables;
 
 END //
