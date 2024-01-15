c71fc897c406d3cde21db8a2c3e91b764d6c4aa5
smallGod <davies.mugume@gmail.com>
2024-01-15 08:48:17 +0300

    OHRI-2102 Add OpenMRS license header text

--- api/src/main/java/org/openmrs/module/ohrimambacore/api/model/MambaReportItem.java
+++ api/src/main/java/org/openmrs/module/ohrimambacore/api/model/MambaReportItem.java
@@ -1,6 +1,13 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * <p>
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.ohrimambacore.api.model;
-
-import org.openmrs.BaseOpenmrsObject;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -12,8 +19,6 @@ import java.util.List;
 public class MambaReportItem implements Serializable {
 
     private static final long serialVersionUID = -8980686055469327763L;
-
-//    private MambaReportItemMetadata metaData;
 
     private Integer serialId;
 
--- api/src/main/java/org/openmrs/module/ohrimambacore/db/ConnectionPoolManager.java
+++ api/src/main/java/org/openmrs/module/ohrimambacore/db/ConnectionPoolManager.java
@@ -1,31 +1,40 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * <p>
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
 package org.openmrs.module.ohrimambacore.db;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.openmrs.api.context.Context;
 
 public class ConnectionPoolManager {
-	
-	private static ConnectionPoolManager instance = null;
-	
-	private static final BasicDataSource dataSource = new BasicDataSource();
-	
-	private ConnectionPoolManager() {
-		dataSource.setDriverClassName(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.driver"));
-		dataSource.setUrl(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.url"));
-		dataSource.setUsername(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.username"));
-		dataSource.setPassword(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.password"));
-		dataSource.setInitialSize(4); // Initial number of connections
-		dataSource.setMaxTotal(20); // Maximum number of connections
-	}
-	
-	public static ConnectionPoolManager getInstance() {
-		if (instance == null) {
-			instance = new ConnectionPoolManager();
-		}
-		return instance;
-	}
-	
-	public BasicDataSource getDataSource() {
-		return dataSource;
-	}
+
+    private static ConnectionPoolManager instance = null;
+
+    private static final BasicDataSource dataSource = new BasicDataSource();
+
+    private ConnectionPoolManager() {
+        dataSource.setDriverClassName(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.driver"));
+        dataSource.setUrl(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.url"));
+        dataSource.setUsername(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.username"));
+        dataSource.setPassword(Context.getAdministrationService().getGlobalProperty("mambaetl.analysis.db.password"));
+        dataSource.setInitialSize(4); // Initial number of connections
+        dataSource.setMaxTotal(20); // Maximum number of connections
+    }
+
+    public static ConnectionPoolManager getInstance() {
+        if (instance == null) {
+            instance = new ConnectionPoolManager();
+        }
+        return instance;
+    }
+
+    public BasicDataSource getDataSource() {
+        return dataSource;
+    }
 }
