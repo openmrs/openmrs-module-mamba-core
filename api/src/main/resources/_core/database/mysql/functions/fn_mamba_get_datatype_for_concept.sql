bf17c5887163423ae9bd0a16c494445fdc2a7ae1
laureen <laureen.omare@ucgloballprograms.org>
2024-03-04 09:27:49 +0300

    OHRI-1787

--- api/src/main/resources/_core/database/mysql/functions/fn_mamba_get_datatype_for_concept.sql
+++ api/src/main/resources/_core/database/mysql/functions/fn_mamba_get_datatype_for_concept.sql
@@ -7,26 +7,23 @@ CREATE FUNCTION fn_mamba_get_datatype_for_concept(conceptDatatype VARCHAR(20)) R
 BEGIN
     DECLARE mysqlDatatype VARCHAR(20);
 
-    IF conceptDatatype = 'Coded'
-       OR conceptDatatype = 'N/A' THEN
-        SET mysqlDatatype = 'TEXT';
+    CASE conceptDatatype
+        WHEN conceptDatatype IN('Coded','N/A','Text') THEN
+            SET mysqlDatatype = 'TEXT';
 
-    ELSEIF conceptDatatype = 'Text' THEN
-        SET mysqlDatatype = 'TEXT';
+        WHEN conceptDatatype = 'Boolean' THEN
+            SET mysqlDatatype = 'VARCHAR(20)';
 
-    ELSEIF conceptDatatype = 'Boolean' THEN
-        SET mysqlDatatype = 'VARCHAR(30)';
+        WHEN conceptDatatype = 'Date' THEN
+            SET mysqlDatatype = 'DATE';
 
-    ELSEIF conceptDatatype = 'Date' THEN
-        SET mysqlDatatype = 'DATE';
+        WHEN conceptDatatype = 'Datetime' THEN
+            SET mysqlDatatype = 'DATETIME';
 
-    ELSEIF conceptDatatype = 'Datetime' THEN
-        SET mysqlDatatype = 'DATETIME';
+        WHEN conceptDatatype = 'Numeric' THEN
+            SET mysqlDatatype = 'DOUBLE';
 
-    ELSEIF conceptDatatype = 'Numeric' THEN
-        SET mysqlDatatype = 'DOUBLE';
-
-    END IF;
+    END CASE;
 
     RETURN mysqlDatatype;
 END //
