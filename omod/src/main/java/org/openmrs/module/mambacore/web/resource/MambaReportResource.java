3e85450bc0d81e85ad934dbb95c25f648b43e277
smallGod <davies.mugume@gmail.com>
2024-03-19 12:05:52 +0300

    OpenMRS readiness changes

--- omod/src/main/java/org/openmrs/module/mambacore/web/resource/MambaReportResource.java
+++ omod/src/main/java/org/openmrs/module/mambacore/web/resource/MambaReportResource.java
@@ -28,10 +28,12 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 import java.util.Enumeration;
 import java.util.List;
 
-@Resource(name = RestConstants.VERSION_1 + MambaReportRestController.MAMBA_REPORT_REST_NAMESPACE + "/report", supportedClass = MambaReportItem.class, supportedOpenmrsVersions = {"2.0 - 2.*"})
+@Resource(name = RestConstants.VERSION_1 + MambaReportRestController.MAMBA_REPORT_REST_NAMESPACE + "/report",
+        supportedClass = MambaReportItem.class,
+        supportedOpenmrsVersions = {"2.0 - 2.*"})
 public class MambaReportResource implements Searchable {
 
-    private static Logger log = LoggerFactory.getLogger(MambaReportResource.class);
+    private static final Logger log = LoggerFactory.getLogger(MambaReportResource.class);
 
     private MambaReportService getService() {
         return Context.getService(MambaReportService.class);
