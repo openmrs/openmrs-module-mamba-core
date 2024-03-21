9101ea7eb604c8499892fc461e83f72529916e2b
laureen <laureen.omare@ucgloballprograms.org>
2024-03-21 09:27:31 +0300

    OHRI-2176 change to use of uuid

--- api/src/main/resources/_core/database/mysql/z/obs/sp_mamba_z_encounter_obs_update.sql
+++ api/src/main/resources/_core/database/mysql/z/obs/sp_mamba_z_encounter_obs_update.sql
@@ -17,21 +17,22 @@ SET z.obs_value_text       = cn.name,
     z.obs_value_coded_uuid = c.uuid
 WHERE z.obs_value_coded IS NOT NULL;
 
--- Add and update column obs_value_boolean (UUIDs & Concept value names)
+-- Add and update column obs_value_boolean (Concept values)
 ALTER TABLE mamba_z_encounter_obs
-    ADD obs_value_boolean boolean;
+ADD obs_value_boolean boolean;
 
 UPDATE   mamba_z_encounter_obs z
-    INNER JOIN mamba_dim_concept_name cn
-ON z.obs_value_coded = cn.concept_id
-    INNER JOIN mamba_dim_concept c
+INNER JOIN mamba_dim_concept_name cn
+    ON z.obs_value_coded = cn.concept_id
+INNER JOIN mamba_dim_concept c
     ON c.concept_id = cn.concept_id
-    INNER JOIN mamba_dim_concept_datatype cd
+INNER JOIN mamba_dim_concept_datatype cd
     ON c.datatype_id = cd.concept_datatype_id
-    SET obs_value_boolean =
-        CASE WHEN obs_value_coded in (1,88) THEN TRUE
-            WHEN obs_value_coded in (2,1980) THEN FALSE
-        END
+SET obs_value_boolean =
+    CASE
+        WHEN obs_value_coded in (SELECT DISTINCT concept_id FROM mamba_dim_concept c where c.uuid in ('cf82933b-3f3f-45e7-a5ab-5d31aaee3da3','1065AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')) THEN TRUE
+        WHEN obs_value_coded in (SELECT DISTINCT concept_id FROM mamba_dim_concept c where c.uuid in ('488b58ff-64f5-4f8a-8979-fa79940b1594','1066AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')) THEN FALSE
+    END
 WHERE z.obs_value_coded IS NOT NULL;
 
 
