53967ba6be2cf5f27f3f8572b53aa8788a3579dd
smallGod <davies.mugume@gmail.com>
2024-04-17 09:55:10 +0300

    correct driver class property

--- api/src/main/java/org/openmrs/module/mambacore/db/ConnectionPoolManager.java
+++ api/src/main/java/org/openmrs/module/mambacore/db/ConnectionPoolManager.java
@@ -15,38 +15,38 @@ import org.openmrs.api.context.Context;
 import java.util.Properties;
 
 public class ConnectionPoolManager {
-
-    private static ConnectionPoolManager instance = null;
-
-    private static final BasicDataSource dataSource = new BasicDataSource();
-
-    private ConnectionPoolManager() {
-
-        Properties omrsRuntimeProperties = Context.getRuntimeProperties();
-
-        String driver = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.driver");
-        String url = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.url");
-        String userName = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.username");
-        String password = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.password");
-        String defaultUrl = "jdbc:mysql://localhost:3306/analysis_db?autoReconnect=true&useSSL=false";
-
-        dataSource.setDriverClassName(driver != null ? driver : omrsRuntimeProperties.getProperty("connection.driver"));
-        dataSource.setUsername(userName != null ? userName : omrsRuntimeProperties.getProperty("connection.username"));
-        dataSource.setPassword((password != null ? password : omrsRuntimeProperties.getProperty("connection.password")));
-        dataSource.setUrl(url != null ? url : defaultUrl);
-
-        dataSource.setInitialSize(4); // Initial number of connections
-        dataSource.setMaxTotal(20); // Maximum number of connections
-    }
-
-    public static ConnectionPoolManager getInstance() {
-        if (instance == null) {
-            instance = new ConnectionPoolManager();
-        }
-        return instance;
-    }
-
-    public BasicDataSource getDataSource() {
-        return dataSource;
-    }
+	
+	private static ConnectionPoolManager instance = null;
+	
+	private static final BasicDataSource dataSource = new BasicDataSource();
+	
+	private ConnectionPoolManager() {
+		
+		Properties omrsRuntimeProperties = Context.getRuntimeProperties();
+		
+		String driver = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.driver");
+		String url = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.url");
+		String userName = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.username");
+		String password = omrsRuntimeProperties.getProperty("mambaetl.analysis.db.password");
+		String defaultUrl = "jdbc:mysql://localhost:3306/analysis_db?autoReconnect=true&useSSL=false";
+		
+		dataSource.setDriverClassName(driver != null ? driver : omrsRuntimeProperties.getProperty("connection.driver_class"));
+		dataSource.setUsername(userName != null ? userName : omrsRuntimeProperties.getProperty("connection.username"));
+		dataSource.setPassword((password != null ? password : omrsRuntimeProperties.getProperty("connection.password")));
+		dataSource.setUrl(url != null ? url : defaultUrl);
+		
+		dataSource.setInitialSize(4); // Initial number of connections
+		dataSource.setMaxTotal(20); // Maximum number of connections
+	}
+	
+	public static ConnectionPoolManager getInstance() {
+		if (instance == null) {
+			instance = new ConnectionPoolManager();
+		}
+		return instance;
+	}
+	
+	public BasicDataSource getDataSource() {
+		return dataSource;
+	}
 }
