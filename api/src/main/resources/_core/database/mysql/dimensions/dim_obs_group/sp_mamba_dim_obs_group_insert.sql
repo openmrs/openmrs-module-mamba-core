70a6a0c27c59d8278e30605b3e9e7409e8ff39ed
laureen <laureen.omare@ucgloballprograms.org>
2024-05-07 10:09:02 +0300

    Remove partition by and json specific code for mysql57

--- api/src/main/resources/_core/database/mysql/dimensions/dim_obs_group/sp_mamba_dim_obs_group_insert.sql
+++ api/src/main/resources/_core/database/mysql/dimensions/dim_obs_group/sp_mamba_dim_obs_group_insert.sql
@@ -12,10 +12,17 @@ FROM mamba_z_encounter_obs o
 WHERE obs_id in
       (SELECT obs_group_id
        FROM (SELECT DISTINCT obs_group_id,
-                             COUNT(*) OVER (PARTITION BY person_id,encounter_id,obs_group_id) row_num
+                             (SELECT COUNT(*)
+                              FROM mamba_z_encounter_obs o2
+                              WHERE o2.obs_group_id = o.obs_group_id
+                                AND o2.person_id = o.person_id
+                                AND o2.encounter_id = o.encounter_id
+                             ) AS row_num
              FROM mamba_z_encounter_obs o
-             WHERE obs_group_id IS NOT NULL) a
-       WHERE row_num > 1)
+             WHERE obs_group_id IS NOT NULL
+            ) a
+       WHERE row_num > 1
+      )
   AND cn.locale IN (SELECT DISTINCT(locale) FROM mamba_dim_locale)
   AND cn.locale_preferred = 1
   AND cn.voided = 0;
