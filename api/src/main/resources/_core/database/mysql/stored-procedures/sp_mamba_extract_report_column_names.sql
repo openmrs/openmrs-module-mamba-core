c04f1c6dbd55e493aec8d4cbcb8a8ef6aedb638d
smallGod <davies.mugume@gmail.com>
2024-05-17 00:59:42 +0300

    remove unncessary selects

--- api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_extract_report_column_names.sql
+++ api/src/main/resources/_core/database/mysql/stored-procedures/sp_mamba_extract_report_column_names.sql
@@ -18,32 +18,22 @@ BEGIN
             LEAVE read_loop;
         END IF;
 
-        -- Output the value of proc_name
-        SELECT proc_name as procedure_name;
-
         -- Fetch the parameters for the procedure and provide empty string values for each
         SET @params := NULL;
+
         SELECT GROUP_CONCAT('\'\'' SEPARATOR ', ')
         INTO @params
         FROM mamba_dim_report_definition_parameters rdp
                  INNER JOIN mamba_dim_report_definition rd on rdp.report_id = rd.report_id
         WHERE rd.report_columns_procedure_name = proc_name;
 
-        -- Output the value of @params
-        SELECT @params as parameters;
-
         IF @params IS NULL THEN
-            SELECT @s as is_null_here;
-            SET @s = CONCAT('CALL ', proc_name, '();');
+            SET @procedure_call = CONCAT('CALL ', proc_name, '();');
         ELSE
-            SET @s = CONCAT('CALL ', proc_name, '(', @params, ');');
-            SELECT @s as is_not_null_here;
+            SET @procedure_call = CONCAT('CALL ', proc_name, '(', @params, ');');
         END IF;
 
-        -- Output the value of @s
-        SELECT @s as procedure_call;
-
-        PREPARE stmt FROM @s;
+        PREPARE stmt FROM @procedure_call;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
     END LOOP;
