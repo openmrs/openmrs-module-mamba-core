b9bc636f634d4acaa614ea7a5fb9a88500bf6850
smallgod <davies.mugume@gmail.com>
2024-08-14 23:05:38 +0200

    improved performance slightly in the z update statement

--- api/src/main/resources/_core/database/mysql/z/obs/sp_mamba_z_encounter_obs_update.sql
+++ api/src/main/resources/_core/database/mysql/z/obs/sp_mamba_z_encounter_obs_update.sql
@@ -1,11 +1,21 @@
 -- $BEGIN
 
+CREATE TEMPORARY TABLE mamba_temp_value_coded_values AS
+SELECT m.concept_id AS concept_id,
+       m.uuid       AS concept_uuid,
+       m.name       AS concept_name
+FROM mamba_dim_concept m
+WHERE concept_id in (SELECT DISTINCT obs_value_coded
+                     FROM mamba_z_encounter_obs
+                     WHERE obs_value_coded IS NOT NULL);
+CREATE INDEX mamba_idx_concept_id ON mamba_temp_value_coded_values (concept_id);
+
 -- update obs_value_coded (UUIDs & Concept value names)
 UPDATE mamba_z_encounter_obs z
-    INNER JOIN mamba_dim_concept c
-    ON z.obs_value_coded = c.concept_id
-SET z.obs_value_text       = c.name,
-    z.obs_value_coded_uuid = c.uuid
+    INNER JOIN mamba_temp_value_coded_values mtv
+    ON z.obs_value_coded = mtv.concept_id
+SET z.obs_value_text       = mtv.concept_name,
+    z.obs_value_coded_uuid = mtv.concept_uuid
 WHERE z.obs_value_coded IS NOT NULL;
 
 -- update column obs_value_boolean (Concept values)
@@ -21,5 +31,7 @@ WHERE z.obs_value_coded IS NOT NULL
       (SELECT DISTINCT concept_id
        FROM mamba_dim_concept c
        WHERE c.datatype = 'Boolean');
+
+DROP TEMPORARY TABLE IF EXISTS mamba_temp_value_coded_values;
 
 -- $END
