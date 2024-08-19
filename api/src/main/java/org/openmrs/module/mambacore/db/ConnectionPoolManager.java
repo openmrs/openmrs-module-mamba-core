15ce9d3fde51a2143062d017039c9580183e3b92
smallgod <davies.mugume@gmail.com>
2024-08-19 16:22:38 +0300

    Correctly switch db connection to analysis_db

--- api/src/main/java/org/openmrs/module/mambacore/db/ConnectionPoolManager.java
+++ api/src/main/java/org/openmrs/module/mambacore/db/ConnectionPoolManager.java
@@ -13,37 +13,51 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.openmrs.module.mambacore.util.MambaETLProperties;
 
 public class ConnectionPoolManager {
-	
-	private static ConnectionPoolManager instance = null;
-	
-	private static final BasicDataSource dataSource = new BasicDataSource();
-	
-	private final MambaETLProperties props = MambaETLProperties.getInstance();
-	
-	private ConnectionPoolManager() {
-		
-		dataSource.setDriverClassName(props.getOpenmrsDbDriver());
-		dataSource.setUsername(props.getMambaETLuser());
-		dataSource.setPassword(props.getMambaETLuserPassword());
-		dataSource.setUrl(props.getOpenmrsDbConnectionUrl());
-		
-		dataSource.setInitialSize(props.getConnectionInitialSize());
-		dataSource.setMaxTotal(props.getConnectionMaxTotal());
-	}
-	
-	public static ConnectionPoolManager getInstance() {
-		if (instance == null) {
-			instance = new ConnectionPoolManager();
-		}
-		return instance;
-	}
-	
-	public BasicDataSource getDefaultDataSource() {
-		return dataSource;
-	}
-	
-	public BasicDataSource getEtlDataSource() {
-		dataSource.setDefaultSchema(props.getEtlDatababase());
-		return dataSource;
-	}
+
+    private static final BasicDataSource dataSource = new BasicDataSource();
+    private static ConnectionPoolManager instance = null;
+    private final MambaETLProperties props = MambaETLProperties.getInstance();
+
+    private ConnectionPoolManager() {
+
+        dataSource.setDriverClassName(props.getOpenmrsDbDriver());
+        dataSource.setUsername(props.getMambaETLuser());
+        dataSource.setPassword(props.getMambaETLuserPassword());
+        dataSource.setUrl(props.getOpenmrsDbConnectionUrl());
+
+        dataSource.setInitialSize(props.getConnectionInitialSize());
+        dataSource.setMaxTotal(props.getConnectionMaxTotal());
+    }
+
+    public static ConnectionPoolManager getInstance() {
+        if (instance == null) {
+            instance = new ConnectionPoolManager();
+        }
+        return instance;
+    }
+
+    public BasicDataSource getDefaultDataSource() {
+        return dataSource;
+    }
+
+    public BasicDataSource getEtlDataSource() {
+
+        String etlDatabase = props.getEtlDatababase();
+        String modifiedUrl = getModifiedUrl(dataSource.getUrl(), etlDatabase);
+
+        BasicDataSource etlDataSource = new BasicDataSource();
+        etlDataSource.setDefaultSchema(etlDatabase);
+        etlDataSource.setUrl(modifiedUrl);
+        etlDataSource.setDriverClassName(dataSource.getDriverClassName());
+        etlDataSource.setUsername(props.getMambaETLuser());
+        etlDataSource.setPassword(props.getMambaETLuserPassword());
+        etlDataSource.setInitialSize(dataSource.getInitialSize());
+        etlDataSource.setMaxTotal(dataSource.getMaxTotal());
+
+        return etlDataSource;
+    }
+
+    private String getModifiedUrl(String originalUrl, String newDatabase) {
+        return originalUrl.replaceAll("(/)[^/?]+(?=\\?|$)", "$1" + newDatabase);
+    }
 }
