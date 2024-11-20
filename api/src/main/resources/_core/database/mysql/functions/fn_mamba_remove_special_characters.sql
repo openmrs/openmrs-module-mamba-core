b8841204b0ff9abbaef421696b28ac70d50a534a
smallgod <davies.mugume@gmail.com>
2024-11-20 09:20:49 +0300

    fix issue with handling of special characters

--- api/src/main/resources/_core/database/mysql/functions/fn_mamba_remove_special_characters.sql
+++ api/src/main/resources/_core/database/mysql/functions/fn_mamba_remove_special_characters.sql
@@ -1,26 +1,37 @@
-DROP FUNCTION IF EXISTS fn_mamba_remove_special_characters;
-
 DELIMITER //
 
+DROP FUNCTION IF EXISTS fn_mamba_remove_special_characters //
+
 CREATE FUNCTION fn_mamba_remove_special_characters(input_text VARCHAR(255))
-    RETURNS VARCHAR(255) DETERMINISTIC
+    RETURNS VARCHAR(255)
+    DETERMINISTIC
+    NO SQL
+    COMMENT 'Removes special characters from input text'
 BEGIN
     DECLARE modified_string VARCHAR(255);
     DECLARE special_chars VARCHAR(255);
     DECLARE char_index INT DEFAULT 1;
     DECLARE current_char CHAR(1);
 
-    SET modified_string = input_text;
-    -- SET special_chars = '!@#$%^&*?/,()"-=+£:;><ã';
-    SET special_chars = '!@#$%^&*?/,()"-=+£:;><ã\\|\\[\\]{}\'~'.`'; -- TODO: Added '.' xter as well but Remove after adding backtick support
+    -- Handle null input
+    IF input_text IS NULL THEN
+        RETURN NULL;
+    END IF;
 
+    SET modified_string = input_text;
+
+    -- Define special characters to remove
+    SET special_chars = '!@#$%^&*?/,()"-=+£:;><ã\\|[]{}\'~`.';
+
+    -- Remove each special character
     WHILE char_index <= CHAR_LENGTH(special_chars) DO
             SET current_char = SUBSTRING(special_chars, char_index, 1);
             SET modified_string = REPLACE(modified_string, current_char, '');
             SET char_index = char_index + 1;
         END WHILE;
 
-    RETURN modified_string;
+    -- Trim any leading or trailing spaces
+    RETURN TRIM(modified_string);
 END //
 
 DELIMITER ;
