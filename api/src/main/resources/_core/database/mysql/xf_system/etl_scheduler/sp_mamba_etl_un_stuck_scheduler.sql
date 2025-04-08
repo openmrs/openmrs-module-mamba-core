e1cd4a987583f5a24165606973e8af0a17e7196d
samuelabebayehu <saa2280@cumc.columbia.edu>
2025-04-08 10:35:17 +0300

    simplify check for no running mamba_sp

--- api/src/main/resources/_core/database/mysql/xf_system/etl_scheduler/sp_mamba_etl_un_stuck_scheduler.sql
+++ api/src/main/resources/_core/database/mysql/xf_system/etl_scheduler/sp_mamba_etl_un_stuck_scheduler.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE sp_mamba_etl_un_stuck_scheduler()
 BEGIN
 
     DECLARE running_schedule_record BOOLEAN DEFAULT FALSE;
-    DECLARE running_mamba_sp BOOLEAN DEFAULT FALSE;
+    DECLARE no_running_mamba_sp BOOLEAN DEFAULT FALSE;
     DECLARE last_schedule_record_id INT;
 
     SET running_schedule_record = (SELECT COALESCE(
@@ -16,15 +16,13 @@ BEGIN
                                                    FROM _mamba_etl_schedule
                                                    ORDER BY id DESC
                                                    LIMIT 1), TRUE));
-    SET running_mamba_sp = (SELECT COALESCE(
-                                           (SELECT IF(COUNT(*) = 0, TRUE, FALSE)
-                                            FROM performance_schema.events_statements_current
-                                            WHERE SQL_TEXT = 'CALL sp_mamba_etl_scheduler_wrapper()'
-                                            ORDER BY EVENT_ID DESC
-                                            LIMIT 1), TRUE));
-    IF running_schedule_record AND running_mamba_sp THEN
+    SET no_running_mamba_sp = NOT EXISTS (SELECT 1
+                                          FROM performance_schema.events_statements_current
+                                          WHERE SQL_TEXT LIKE 'CALL sp_mamba_etl_scheduler_wrapper(%'
+                                             OR SQL_TEXT = 'CALL sp_mamba_etl_scheduler_wrapper()');
+    IF running_schedule_record AND no_running_mamba_sp THEN
         SET last_schedule_record_id = (SELECT MAX(id) FROM _mamba_etl_schedule limit 1);
-        DELETE FROM _mamba_etl_schedule WHERE id=last_schedule_record_id;
+        DELETE FROM _mamba_etl_schedule WHERE id = last_schedule_record_id;
     END IF;
 
 END //
