c7ce990e37ee9c131e795042f69eb355f73671b8
smallgod <davies.mugume@gmail.com>
2025-07-08 09:32:06 +0300

    add tracking column

--- api/src/main/resources/_core/database/mysql/xf_system/etl_scheduler/sp_mamba_etl_schedule_table_create.sql
+++ api/src/main/resources/_core/database/mysql/xf_system/etl_scheduler/sp_mamba_etl_schedule_table_create.sql
@@ -16,6 +16,7 @@ BEGIN
         completion_status          ENUM ('SUCCESS', 'ERROR'),
         transaction_status         ENUM ('RUNNING', 'COMPLETED'),
         success_or_error_message   MEDIUMTEXT,
+        start_in INT,
 
         INDEX mamba_idx_start_time (start_time),
         INDEX mamba_idx_end_time (end_time),
